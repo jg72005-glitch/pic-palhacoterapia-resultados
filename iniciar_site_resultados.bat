5ddf0af874c3bb1592a1c030ff54c3b203b2db05
jg72005-glitch <jg72005@gmail.com>
2026-05-15 13:57:53 -0300

    Delete iniciar_site_resultados.bat

--- iniciar_site_resultados.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /d "%~dp0"
-echo Site de resultados rodando em http://127.0.0.1:8788
-echo Feche esta janela somente quando terminar de usar o site.
-start "" "http://127.0.0.1:8788"
-"C:\Users\joaog\.cache\codex-runtimes\codex-primary-runtime\dependencies\node\bin\node.exe" server.js
-pause
