61d3a768f8a0d1b859f574a27957a379eb6012ef
jg72005-glitch <jg72005@gmail.com>
2026-05-15 13:58:43 -0300

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,9 +0,0 @@
-window.PIC_SUPABASE_CONFIG = {
-  enabled: true,
-  url: "https://flydaodpjklyumfkjqgt.supabase.co",
-  anonKey: "sb_publishable_kvy0TodNlAFZelydXir3Mw_hfNADNEq",
-  dataMode: "supabase",
-  allowedEmails: [
-    "jg72005@gmail.com"
-  ]
-};
