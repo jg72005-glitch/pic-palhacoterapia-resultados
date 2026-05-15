cd61c9383cdf2def62a82f25f1436ca46edede56
jg72005-glitch <jg72005@gmail.com>
2026-05-15 13:43:57 -0300

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,7 +1,9 @@
 window.PIC_SUPABASE_CONFIG = {
   enabled: true,
-  url: "https://flydaodpjklyumfkjqgt.supabase.co",
+  url: "https://flydaodpjklyumfkjqgt.supabase.co/rest/v1/",
   anonKey: "sb_publishable_kvy0TodNlAFZelydXir3Mw_hfNADNEq",
   dataMode: "supabase",
-  allowedEmails: []
+  allowedEmails: [
+  "jg72005@gmail.com"
+  ]
 };
